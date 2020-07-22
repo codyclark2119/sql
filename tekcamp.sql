b018c7912555568fd80f44a0ecb0457dcc12f025
cody clark <codyclark2119@gmail.com>
2020-07-21 22:23:25 -0500

    Finished activity 1

--- tekcamp.sql
+++ tekcamp.sql
@@ -1,3 +1,5 @@
+-- Original DB: student
+-- Original Table: tekcamp01
 -- Creation of my database
 CREATE DATABASE `tc1-Cody-Clark`;
 
@@ -17,15 +19,10 @@ CREATE TABLE tekcampers
 );
 
 -- Get data from original database
-INSERT INTO tekcampers
-    (id, first_name, last_name, gender, education)
-SELECT *
-FROM students.tekcamp01;
+INSERT INTO tekcampers (id, first_name, last_name, gender, education) SELECT * FROM students.tekcamp01;
 
 -- Inserting missing students but following original format for data for ease of movement
-INSERT INTO tekcampers
-    (id, first_name, last_name, gender, education)
-VALUES
+INSERT INTO tekcampers (id, first_name, last_name, gender, education) VALUES
     (28, "Aaron", "White", null, "M"),
     (29, "Cody", "Clark", null, "M"),
     (30, "Devon", "Brewster", null, "M"),
@@ -70,92 +67,49 @@ UPDATE tekcampers SET first_name="Matthew" WHERE first_name="Mathew" AND last_na
 UPDATE tekcampers SET first_name="Matthew" WHERE first_name="Mathew" AND last_name="Castiglione";
 
 -- Queries for
---    1. Students whose last names are longer than 7 characters
-SELECT *
-FROM tekcampers
-WHERE CHAR_LENGTH(last_name) > 7;
---    2. Alphabatized by student's first name
-SELECT *
-FROM tekcampers
-ORDER BY first_name asc;
---    3. Alphabatized by student's last name
-SELECT *
-FROM tekcampers
-ORDER BY last_name asc;
---    4. Students that are male
-SELECT *
-FROM tekcampers
-WHERE gender="M";
---    5. Students that are female
-SELECT *
-FROM tekcampers
-WHERE gender="F";
---    6. Students that have a Masters degree
-SELECT *
-FROM tekcampers
-WHERE education="Masters";
---    7. Students that have a Bachelors degree
-SELECT *
-FROM tekcampers
-WHERE education="Bachelors";
---    8. Students that have an Associate degree
-SELECT *
-FROM tekcampers
-WHERE education="Associates";
---    9. Students that don't currently have a Higher Education Degree
-SELECT *
-FROM tekcampers
-WHERE education IN ("High School", "Some College");
+-- 1. Students whose last names are longer than 7 characters
+SELECT * FROM tekcampers WHERE CHAR_LENGTH(last_name) > 7;
+-- 2. Alphabatized by student's first name
+SELECT * FROM tekcampers ORDER BY first_name asc;
+-- 3. Alphabatized by student's last name
+SELECT * FROM tekcampers ORDER BY last_name asc;
+-- 4. Students that are male
+SELECT * FROM tekcampers WHERE gender="M";
+-- 5. Students that are female
+SELECT * FROM tekcampers WHERE gender="F";
+-- 6. Students that have a Masters degree
+SELECT * FROM tekcampers WHERE education="Masters";
+-- 7. Students that have a Bachelors degree
+SELECT * FROM tekcampers WHERE education="Bachelors";
+-- 8. Students that have an Associate degree
+SELECT * FROM tekcampers WHERE education="Associates";
+-- 9. Students that don't currently have a Higher Education Degree
+SELECT * FROM tekcampers WHERE education IN ("High School", "Some College");
 
--- Create table for ta-mark
-CREATE TABLE `ta-mark`
-(
+-- Create table for ta-mark with foreign key for student data relation
+CREATE TABLE `ta-mark` (
     student_id INT,
-    FOREIGN KEY
-(student_id) REFERENCES tekcampers
-(id)
-)
+    FOREIGN KEY (student_id) REFERENCES tekcampers(id)
+);
 -- Migrating specific students to ta-mark table
-INSERT INTO `
-ta-mark
-`
+INSERT INTO `ta-mark`
 (student_id) VALUES
-(8),
-(21),
-(23),
-(25),
-(27),
-(28),
-(29),
-(31),
-(32),
-(34),
-(35),
-(37);
--- Showing the students in ta-marks full information
+(8),(21),(23),(25),(27),(28),(29),(31),(32),(34),(35),(37);
+
+-- Showing the students in ta-mark with full information
 SELECT tekcampers.*
 FROM tekcampers
-    INNER JOIN `ta  -mark
-`
+    INNER JOIN `ta-mark`
 ON tekcampers.id = `ta-mark`.student_id;
 
 -- Create hobbies table
-CREATE TABLE `hobbies`
-(
+CREATE TABLE `hobbies` (
     id INT,
-    name VARCHAR
-(100),
-PRIMARY KEY
-(id)
+    name VARCHAR (100),
+    PRIMARY KEY (id)
 )
 -- Creating a refrence list
-INSERT INTO `
-hobbies
-`
-(
-id,
-name
-) VALUES
+INSERT INTO `hobbies`(id, name) VALUES
 (1, "Reading"),
 (2, "Motorcycle Riding"),
 (3, "Video Games"),
@@ -175,7 +129,7 @@ name
 (17, "Growing Hot Peppers"),
 (18, "Longboarding");
 
--- Adding hobby id to tekcampers table
+-- Adding hobby id to tekcampers table with a null default
 ALTER TABLE tekcampers 
 ADD hobby_id Int null 
 DEFAULT null;
@@ -205,11 +159,23 @@ UPDATE tekcampers SET hobby_id=17 WHERE last_name="Clements";
 UPDATE tekcampers SET hobby_id=19 WHERE last_name="Kim";
 UPDATE tekcampers SET hobby_id=20 WHERE last_name="Radhakrishnan";
 
--- Showing the students in ta-marks full information with hobbies *NOTE: Only returns people with set hobbies values*
+-- Adding Bootcamp field to tekcampers table
+ALTER TABLE tekcampers 
+ADD bootcamp VARCHAR(100) null 
+DEFAULT null;
+-- Setting bootcamp values 
+UPDATE tekcampers SET bootcamp="Coding Dojo" WHERE last_name IN ("Rice", "Ladd", "Barbosa");
+UPDATE tekcampers SET bootcamp="DevMountain" WHERE last_name IN ("Shane", "Johnson");
+UPDATE tekcampers SET bootcamp="Trilogy Education" WHERE last_name IN ("Clark");
+
+-- Setting all nulls to default values
+UPDATE tekcampers SET hobby_id=0 WHERE hobby_id IS NULL;
+UPDATE tekcampers SET education="N/A" WHERE education IS NULL;
+UPDATE tekcampers SET bootcamp="N/A" WHERE bootcamp IS NULL;
+
+-- Showing the students in ta-marks full information with hobbies and bootcamp information
 SELECT tekcampers.*, hobbies.name
 FROM tekcampers
-    INNER JOIN `ta  -mark
-`
+    INNER JOIN `ta-mark`
 ON tekcampers.id = `ta-mark`.student_id
 INNER JOIN hobbies ON tekcampers.hobby_id = hobbies.id;
-
